d42203a5667b43dc253f8e1f6964df460c3dab17
Helder Bruno Araujo Silva <hbas.developer@gmail.com>
2019-12-11 15:04:46 -0500

    Data

--- taskbox/src/components/TaskList.js
+++ taskbox/src/components/TaskList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { action } from '@storybook/addon-actions'
 
 import Task from "./Task";
+import { connect } from "react-redux";
+import { archiveTask, pinTask } from "../lib/redux";
 
-function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   const events = {
     onPinTask,
     onArchiveTask
@@ -58,17 +59,25 @@ function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   );
 }
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
   loading: PropTypes.bool,
   tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
   onPinTask: PropTypes.func.isRequired,
   onArchiveTask: PropTypes.func.isRequired
 };
 
-TaskList.defaultProps = {
-  loading: false,
-  onPinTask: action('onPinTask'),
-  onArchiveTask: action('onArchiveTask')
+PureTaskList.defaultProps = {
+  loading: false
 };
 
-export default TaskList;
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(
+      t => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+    )
+  }),
+  dispatch => ({
+    onArchiveTask: id => dispatch(archiveTask(id)),
+    onPinTask: id => dispatch(pinTask(id))
+  })
+)(PureTaskList);
