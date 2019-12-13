fb2fa93e410edd3940a6cf75cc74963e16b22310
Helder Bruno Araujo Silva <hbas.developer@gmail.com>
2019-12-13 12:33:42 -0500

    Creating addons

--- taskbox/src/components/TaskList.test.js
+++ taskbox/src/components/TaskList.test.js
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import PureTaskList from "./TaskList";
-import { withPinnedTasks } from "./TaskList.stories";
+import { withPinnedTasks, defaultTasks } from "./TaskList.stories";
+import { action } from "@storybook/addon-actions";
+import { Provider } from "react-redux";
+
+const store = {
+  getState: () => {
+    return {
+      tasks: withPinnedTasks
+    };
+  },
+  subscribe: () => 0,
+  dispatch: action("dispatch")
+};
 
 it("renders pinned tasks at the start of the list", () => {
   const div = document.createElement("div");
   const events = { onPinTask: jest.fn(), onArchiveTask: jest.fn() };
-  ReactDOM.render(<PureTaskList tasks={withPinnedTasks} {...events} />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <PureTaskList tasks={withPinnedTasks} {...events} />
+    </Provider>,
+    div
+  );
 
   // We expect the task titled "Task 6 (pinned)" to be rendered first, not at the end
   const lastTaskInput = div.querySelector(
